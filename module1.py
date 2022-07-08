2e8686d1064dc08e6560e03fb8563938579c0538
SurajKumar861997 <108599873+SurajKumar861997@users.noreply.github.com>
2022-07-08 20:00:43 +0530

    Add files via upload

---
+++ module1.py
@@ -0,0 +1,129 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      hp1
+#
+# Created:     27-05-2022
+# Copyright:   (c) hp1 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+#even odd
+##num = (1,2,3,4,5,6,7,8,9)
+##count_even=0
+##count_odd=0
+##for i in num:
+##    if not i%2:
+##        count_even += 1
+##    else:
+##        count_odd += 1
+##print ('number of even number', count_even)
+##print ('number of odd number', count_odd)
+###new problem
+##num in range (1,50)
+##Buzz = 0
+##Fizz = 0
+##BuzzFizz = 0
+##for i in num:
+##    if i%3==0:
+##        Buzz = 1
+##    elif i%5==0:
+##        Fizz = 1
+##    else:
+##        i%3==0 and i%5==0
+##        BuzzFizz = 1
+##print('Buzz','Fizz','BuzzFizz')
+##
+
+
+no = 20
+print(id(no))
+no = 30
+print (id(no))
+0
+
+##na = 1000.2
+##print (id(na))
+##na1 = 1000.2
+##print (id(na1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
